e21458d91132f9b6e184b4811a8e6d470221b337
Fox <279099862@qq.com>
2017-05-23 03:33:48 +0800

    闪屏跳转GuideActivity。注意windowSoftInputMode设置输入键盘属性

---
+++ WeChat/src/cn/ucai/wechat/ui/GuideActivity.java
@@ -0,0 +1,18 @@
+package cn.ucai.wechat.ui;
+
+import android.os.Bundle;
+
+import cn.ucai.wechat.R;
+
+/**
+ * Created by apple on 2017/5/23.
+ */
+
+public class GuideActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_guide);
+    }
+}
