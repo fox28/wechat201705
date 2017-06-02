ee2e7b03f06f4e046752e1cb8f72095223ec7f0b
Fox <279099862@qq.com>
2017-06-02 13:23:35 +0800

    day7.07联系人列表点击进入用户详情

--- WeChat/src/cn/ucai/wechat/ui/ContactListFragment.java
+++ WeChat/src/cn/ucai/wechat/ui/ContactListFragment.java
@@ -117,11 +117,12 @@ public class ContactListFragment extends EaseContactListFragment {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                EaseUser user = (EaseUser)listView.getItemAtPosition(position);
+                User user = (User) listView.getItemAtPosition(position);
                 if (user != null) {
-                    String username = user.getUsername();
+                    String username = user.getMUserName();
                     // demo中直接进入聊天页面，实际一般是进入用户详情页
-                    startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("userId", username));
+                    MFGT.gotoFriendProfileActivity(getActivity(), user);
+//                    startActivity(new Intent(getActivity(), ChatActivity.class).putExtra("userId", username));
                 }
             }
         });
